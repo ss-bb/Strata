5b1de63b358843acdad9c5c1bfc8bbe65a90693b
olivier <goyon.olivier@gmail.com>
2015-11-23 13:38:19 +0100

    test Strata

---
+++ app/app.js
@@ -0,0 +1,25 @@
+﻿var app = angular.module('speakersApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/speakers',
+            {
+                controller: 'SpeakersController',
+                templateUrl: 'app/partials/speakers.html'
+            })
+        .when('/speakerstages/:speakerID',
+            {
+                controller: 'SpeakerStagesController',
+                templateUrl: 'app/partials/speakerStages.html'
+            })
+        .when('/stages',
+            {
+                controller: 'StagesController',
+                templateUrl: 'app/partials/stages.html'
+            })
+        .otherwise({ redirectTo: '/speakers' });
+});
+
+
+
+
