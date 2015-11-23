06c78048ef32661d0e282ca0ccf6ccbc14f1ff9b
olivier <goyon.olivier@gmail.com>
2015-11-23 13:31:26 +0100

    add the project on github to create GH pages
    add the project on github to create GH pages

---
+++ app/controllers/controllers.js
@@ -0,0 +1,82 @@
+﻿
+app.controller('SpeakersController', function ($scope, speakersService) {
+
+    init();
+
+    function init() {
+        $scope.speakers = speakersService.getSpeakers();
+    }
+
+    $scope.insertSpeaker = function () {
+        var firstName = $scope.newSpeaker.firstName;
+        var lastName = $scope.newSpeaker.lastName;
+        var pres = $scope.newSpeaker.pres;
+        speakersService.insertSpeaker(firstName, lastName, pres);
+        $scope.newSpeaker.firstName = '';
+        $scope.newSpeaker.lastName = '';
+        $scope.newSpeaker.pres = '';
+    };
+
+    $scope.deleteSpeaker = function (id) {
+        speakersService.deleteSpeaker(id);
+    };
+});
+
+
+app.controller('SpeakerStagesController', function ($scope, $routeParams, speakersService) {
+    $scope.speaker = {};
+    $scope.stagesTotal = 0.00;
+
+
+    init();
+
+    function init() {     
+        var speakerID = ($routeParams.speakerID) ? parseInt($routeParams.speakerID) : 0;
+        if (speakerID > 0) {
+            $scope.speaker = speakersService.getSpeaker(speakerID);
+        }
+    }
+
+});
+
+
+app.controller('StagesController', function ($scope, speakersService) {
+    $scope.speakers = [];
+    
+    init();
+
+    function init() {
+        $scope.speakers = speakersService.getSpeakers();
+    }
+});
+
+app.controller('NavbarController', function ($scope, $location) {
+    $scope.getClass = function (path) {
+        if ($location.path().substr(0, path.length) == path) {
+            return true
+        } else {
+            return false;
+        }
+    }
+});
+
+//This controller is a child controller that will inherit functionality from a parent
+app.controller('StageChildController', function ($scope) {
+    $scope.stageby = 'speak';
+    $scope.reverse = false;
+    $scope.stagesTotal = 0.00;
+
+    init();
+
+    function init() {
+    }
+
+    $scope.setStage = function (stageby) {
+        if (stageby === $scope.stageby)
+        {
+            $scope.reverse = !$scope.reverse;
+        }
+        $scope.stageby = stageby;
+    };
+
+});
---
+++ app/services/speakersService.js
@@ -0,0 +1,69 @@
+﻿app.service('speakersService', function () {
+    this.getSpeakers = function () {
+        return speakers;
+    };
+
+    this.insertSpeaker = function (firstName, lastName, pres) {
+        var topID = speakers.length + 1;
+        speakers.push({
+            id: topID,
+            firstName: firstName,
+            lastName: lastName,
+            pres: pres
+        });
+    };
+
+    this.deleteSpeaker = function (id) {
+        for (var i = speakers.length - 1; i >= 0; i--) {
+            if (speakers[i].id === id) {
+                speakers.splice(i, 1);
+                break;
+            }
+        }
+    };
+
+    this.getSpeaker = function (id) {
+        for (var i = 0; i < speakers.length; i++) {
+            if (speakers[i].id === id) {
+                return speakers[i];
+            }
+        }
+        return null;
+    };
+
+    // For test purpose, I have no WebService to connect with / Would need more time. So instead, this shoud be an AJAX call in real project.
+    var speakers = [
+        {
+            id: 1, firstName: 'Alice', lastName: 'Alice', address: 'au pays des merveille', pres: 'Clermont',
+            stages: [
+                { speak: 'SVN', day:"monday", stage: 1 },
+                { speak: 'GIT', day:"tuesday", stage: 4 },
+                { speak: 'open Source World', day:"wednesday", stage: 3}
+            ]
+        },
+        {
+            id: 2, firstName: 'Bob', lastName: 'lennon', address: 'heaven', pres: 'Clermont-ferrand',
+            stages: [
+                { speak: 'Javascript power', day:"monday", stage: 1 },
+                { speak: 'Angular', day:"monday", stage: 2},
+                { speak: 'ReactJs', day:"tuesday", stage: 3},
+                { speak: 'NodeJS', day:"wednesday", stage: 2 }
+            ]
+        },
+        {
+            id: 3, firstName: 'Charlie', lastName: 'hedbo', address: 'Lorem ipsum dolor', pres: 'sit amet',
+            stages: [
+                { speak: 'PHP', day:"monday", stage: 4},
+                { speak: 'SF2', day:"tuesday", stage: 1}
+            ]
+        },
+        {
+            id: 4, firstName: 'David', lastName: 'Strettle', address: 'sed do eiusmod tempor', pres: 'ASM',
+            stages: [
+                { speak: 'lorem', day:"monday", stage: 1},
+                { speak: 'Ipsum', day:"wednesday", stage: 2}
+            ]
+        },
+    ];
+
+});
